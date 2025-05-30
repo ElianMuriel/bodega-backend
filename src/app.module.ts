aeb9944d02b21ce1a38e6f79a7045c775c9b2227
Elian Muriel <elian_2546@hotmail.com>
2025-05-30 00:44:17 -0500

    Creado los modulos principales

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { PostsModule } from './posts/posts.module';
+import { ProductsModule } from './products/products.module';
+import { MovementsModule } from './movements/movements.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CategoriesModule, PostsModule, ProductsModule, MovementsModule, SuppliersModule],
   controllers: [AppController],
   providers: [AppService],
 })
