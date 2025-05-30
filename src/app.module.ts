6e69d23f848bf6f9498e5fb41ca720aa303ced5b
Elian Muriel <elian_2546@hotmail.com>
2025-05-30 01:20:40 -0500

    Modificaciones en el app.modules e insatalacion de paquetes necesarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,32 @@ import { PostsModule } from './posts/posts.module';
 import { ProductsModule } from './products/products.module';
 import { MovementsModule } from './movements/movements.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, CategoriesModule, PostsModule, ProductsModule, MovementsModule, SuppliersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    }),
+    AuthModule,
+    UsersModule,
+    CategoriesModule,
+    PostsModule,
+    ProductsModule,
+    MovementsModule,
+    SuppliersModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
