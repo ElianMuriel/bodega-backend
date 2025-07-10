fc44633dd48e91ff11fba6065200b5c24a3549ef
Elian Muriel <elian_2546@hotmail.com>
2025-07-10 03:27:33 -0500

    modificaciones al main para que escuche al puerto del front

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 👇 Habilitar CORS para permitir llamadas desde el frontend
+  app.enableCors({
+    origin: 'http://localhost:5173', // 🔥 tu frontend
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3004);
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('products')
 export class ProductsController {}
